98916eb0f0d8b970e9ac7704c77267c1040c4e67
jeovaneSantos <60937124+jeovaneSantos@users.noreply.github.com>
2020-02-14 17:25:48 -0300

    Create 09_LISTA_1.py

---
+++ Curso de Python/09_LISTA_1.py
@@ -0,0 +1,7 @@
+lista = [("nome: joão","idade:",12) ,("nome: carlos","idade:",15) , ("nome: maria","idade:",16)]
+#lista2=[]
+#tupla = tuple(lista)
+
+#lista2.append(tupla)
+for i, j,k in lista:
+  print(i,j,k)
