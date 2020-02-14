9f19caef98d65be60d5ec52013618614a6b3d3fe
jeovaneSantos <60937124+jeovaneSantos@users.noreply.github.com>
2020-02-14 17:15:51 -0300

    Create 05_OPERADORES.py

---
+++ Curso de Python/05_OPERADORES.py
@@ -0,0 +1,18 @@
+# soma +
+# subtração -
+# multiplicação *
+# divisão /
+# potencia a**b
+# raiz a**b 
+m = 5
+s = 2
+
+soma = m + s
+sub  = m - s
+mul  = m * s
+div  = m / s # s diferete de 0
+pot = m**s
+raiz = pot**(1/2)
+
+print(pot)
+print(raiz)
