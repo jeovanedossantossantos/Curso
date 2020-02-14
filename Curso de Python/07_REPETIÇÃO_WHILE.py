c3808955a48e7219337bc8dfbd2a7e51799ce32a
jeovaneSantos <60937124+jeovaneSantos@users.noreply.github.com>
2020-02-14 17:17:50 -0300

    Create 07_REPETIÇÃO_WHILE.py

---
+++ Curso de Python/07_REPETIÇÃO_WHILE.py
@@ -0,0 +1,19 @@
+# Repetiçao While
+
+cont = 1
+
+while True:
+  print(cont)
+  #cont = cont+1 #soma
+  #cont+=1 #soma
+  #cont = cont-1 
+  #cont -= 1 #subtrai
+  cont *= 2
+
+  op = int(input("Digite 1 ---- Stop"))
+
+  if op == 1:
+    break
+  else:
+    continue
+  
