7d34230d6d1d54f84ec06310770927bc0ddb15af
jeovaneSantos <60937124+jeovaneSantos@users.noreply.github.com>
2020-02-14 17:15:10 -0300

    04_SAIDA.py
    Formatação de saída em python

---
+++ Curso de Python/04_SAIDA.py
@@ -0,0 +1,20 @@
+# soma +
+# subtração -
+# multiplicação *
+# divisão /
+
+m = 20
+s = 2
+
+soma = m + s
+sub  = m - s
+mul  = m * s
+div  = m / s # s diferete de 0
+print(soma,sub,mul,div)
+print("",soma,sub,mul,div)
+print("soma = %.3f\nsubtração = %.2f\nmultiplicação = %.5f\ndivisão = %.6f"%(soma,sub,mul,div))
+#print("%i %d %f %s")
+print("\nFormaut")
+print("soma = {}\nsubtração = {}\nmultiplicação = {}\ndivisão = {}\n\n".format(soma,sub,mul,div))
+print("soma = {:.3f}\nsubtração = {:.2f}\nmultiplicação = {:.5f}\ndivisão = {:.6f}".format(soma,sub,mul,div))
+
