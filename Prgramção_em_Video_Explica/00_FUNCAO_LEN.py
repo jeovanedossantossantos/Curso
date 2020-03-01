b7925c45db6ba9e2dda08195f23c03e593842ae3
jeovaneSantos <60937124+jeovaneSantos@users.noreply.github.com>
2020-03-01 15:01:34 -0300

    00_FUNCAO_LEN.py
    Descobrindo o tamanho de um vetor, strings, tuplas e etc.

---
+++ Prgramção_em_Video_Explica/00_FUNCAO_LEN.py
@@ -0,0 +1,19 @@
+#Função len
+
+lista =[1, 2, 3]
+s = "PROGRAMA"
+t = (1,3,5,7)
+
+tam = 0
+for i in lista:
+  tam+=1
+print(tam)
+print(len(lista))
+tam = 0
+for i in s:
+  tam+=1
+print(tam)
+print(len(s))
+print(len(t))
+
+
